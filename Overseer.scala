7c6ce7faa232025bfacfa3f171c6f05df58363a3
remilvus <remilvus@gmail.com>
2020-05-07 17:17:33 +0200

    init

--- Overseer.scala
+++ Overseer.scala
@@ -1,10 +1,10 @@
 
 
-class Overseer () {
+object Overseer () {
 
     // endless loop
     // training or game based on user input
     // during training one agent may be learning or not
      // when both agents are learning there may only be one instance playing as both
-
+    def main
 }
--- TicTacToe.scala
+++ TicTacToe.scala
@@ -1,12 +1,11 @@
 
 abstract class Environment () {
-    var State
-
-    def step(val action: Int) // returns new state, reward, is_done
+    //var State
+    def step(action: Int) // returns new state, reward, is_done
     def reset()
     def possibleActions() // return bool vector
     def numberOfPossibleActions() : Int 
 
 }
 
-class TicTacToe() extends Environment
+//class TicTacToe() extends Environment
