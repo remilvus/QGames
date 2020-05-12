3d51ace07106efdbf3cdaca71a8c5a863fa79a6e
remilvus <remilvus@gmail.com>
2020-05-12 19:01:26 +0200

    agent update; Overseer main; TTT dummy

--- Overseer.scala
+++ Overseer.scala
@@ -1,10 +1,30 @@
 
 
-object Overseer () {
+object Overseer{
 
     // endless loop
     // training or game based on user input
     // during training one agent may be learning or not
      // when both agents are learning there may only be one instance playing as both
-    def main
+    def main(args: Array[String]){
+        val env : Environment = new TicTacToe()
+        val agent = new Agent(discount=0.9, learning_rate=0.1, environment=env)
+        var state : String = env.reset()
+        var old_state : String = ""
+        var reward : Float = 0.0f
+        var done : Boolean = false
+        var action : Int = 0
+        for (i <- 1 to 10){
+            while(!done){
+                old_state = state
+                action = agent.action(state, env.possibleActions())
+                var (state_1, reward, done_1) = env.step(action)
+                state = state_1
+                done = done_1 // doesnt want to work without it
+                if(!done) agent.update(old_state, state, action, reward)
+            }
+            done=false
+            state = env.reset()
+        }
+    }
 }
--- TicTacToe.scala
+++ TicTacToe.scala
@@ -1,11 +1,16 @@
 
 abstract class Environment () {
     //var State
-    def step(action: Int) // returns new state, reward, is_done
-    def reset()
-    def possibleActions() // return bool vector
+    def step(action: Int) : (String, Float, Boolean) // returns new state, reward, is_done
+    def reset(): String
+    def possibleActions() : Vector[Int] // vector with possible actions
     def numberOfPossibleActions() : Int 
 
 }
 
-//class TicTacToe() extends Environment
+class TicTacToe() extends Environment{
+    def step(action: Int)=  {("x", 1.0f, true)}
+    def reset() = "x"
+    def possibleActions() = {Vector[Int](0)}
+    def numberOfPossibleActions() =  {0}
+}
